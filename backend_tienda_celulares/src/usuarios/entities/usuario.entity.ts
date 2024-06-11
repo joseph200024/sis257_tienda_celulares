b5dca5ff7fe12d6e5fe0eb13844abe554ed751d7
joseph200024 <lokaso669@gmail.com>
2024-06-10 21:23:28 -0400

    se instalo el JWT, y se agrego el directorio auth y se modifico el usuario.entity.ts

--- backend_tienda_celulares/src/usuarios/entities/usuario.entity.ts
+++ backend_tienda_celulares/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,24 @@
-export class Usuario {}
+import * as bcrypt from 'bcrypt';
+export class Usuario {
+  
+
+
+
+
+
+
+  
+  
+  
+  
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    const salt = await bcrypt.genSalt();
+    this.clave = await bcrypt.hash(this.clave, salt);
+  }
+
+  async validatePassword(plainPassword: string): Promise<boolean> {
+    return bcrypt.compare(plainPassword, this.clave);
+  }
+}
